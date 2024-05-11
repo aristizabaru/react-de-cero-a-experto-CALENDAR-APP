6e6f821498dc6e554e871d33a761f9fc12c7a65b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-11 14:51:28 -0500

    refactor (main.jsx): se deshabilita el modo stricto para hacer un debug de consola más eficiente

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CalendarApp } from './CalendarApp.jsx';
 import './style.css';
 
 ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
-  <React.StrictMode>
-    <CalendarApp />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <CalendarApp />
+  // </React.StrictMode>,
 );
