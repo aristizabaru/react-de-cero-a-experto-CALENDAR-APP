1268a56eedff026e4d488bc841bbcebd43a5125d
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-12 05:28:36 -0500

    feat (index.js): se adicionan exports de componentes en archivo de barril
    CalendarModal, FabAddNew y FabDelete

--- src/modules/calendar/index.js
+++ src/modules/calendar/index.js
@@ -1,4 +1,6 @@
-
 export * from './components/CalendarEvent';
+export * from './components/CalendarModal';
 export * from './components/Navbar';
 export * from './pages/CalendarPage';
+export * from './components/FabAddNew';
+export * from './components/FabDelete';
