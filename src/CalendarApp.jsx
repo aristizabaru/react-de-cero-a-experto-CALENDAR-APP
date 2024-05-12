7153950da81eb49fd993c372d5612f201dc7ccb6
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-12 05:27:07 -0500

    feat (CalendarApp.jsx): se implementa provider para redux

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './modules/router';
+import { Provider } from 'react-redux';
+import { store } from './modules/store';
 
 export const CalendarApp = () => {
     return (
-        <BrowserRouter>
-            <AppRouter />
-        </BrowserRouter>
+        <Provider store={ store }>
+            <BrowserRouter>
+                <AppRouter />
+            </BrowserRouter>
+        </Provider>
+
     );
 };
