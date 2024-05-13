6bc4d29097c96b4eae1826de8f2d6f38e7afce1c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-12 20:17:07 -0500

    first commit

---
+++ src/modules/store/calendar/calendarSlice.js
@@ -0,0 +1,58 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const calendarSlice = createSlice( {
+    name: 'calendar',
+    initialState: {
+        isLoadingEvents: true,
+        events: [],
+        activeEvent: null,
+    },
+    reducers: {
+        onSetActiveEvent: ( state, { payload } ) => {
+            state.activeEvent = payload;
+        },
+        onAddNewEvent: ( state, { payload } ) => {
+            state.events.push( payload );
+            state.activeEvent = null;
+        },
+        onUpdateEvent: ( state, { payload } ) => {
+            state.events = state.events.map( event => {
+                return ( event.id === payload.id )
+                    ? payload
+                    : event;
+            } );
+        },
+        onDeleteEvent: ( state ) => {
+            if ( state.activeEvent ) {
+                state.events = state.events.filter( event => event.id !== state.activeEvent.id );
+                state.activeEvent = null;
+            }
+        },
+        onLoadEvents: ( state, { payload = [] } ) => {
+            state.isLoadingEvents = false;
+
+            payload.forEach( event => {
+                const exists = state.events.some( dbEvent => dbEvent.id === event.id );
+                if ( !exists ) {
+                    state.events.push( event );
+                }
+            } );
+        },
+        onLogoutCalendar: ( state ) => {
+            state.isLoadingEvents = true;
+            state.events = [];
+            state.activeEvent = null;
+        },
+    }
+} );
+
+
+// Action creators are generated for each case reducer function
+export const {
+    onAddNewEvent,
+    onDeleteEvent,
+    onLoadEvents,
+    onLogoutCalendar,
+    onSetActiveEvent,
+    onUpdateEvent,
+} = calendarSlice.actions;
