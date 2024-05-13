cdb35b830dc295d208c746415d3500d93ee976a0
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-12 20:21:34 -0500

    feat (Navbar.jsx): implementación de reactividad en navbar
    Se implementa lectura del usuario desde el store y adición de evento al botón del loguout

--- src/modules/calendar/components/Navbar.jsx
+++ src/modules/calendar/components/Navbar.jsx
@@ -1,10 +1,18 @@
+import { useAuthStore } from '../../shared';
+
 export const Navbar = () => {
+
+    const { startLogout, user } = useAuthStore();
+
     return (
         <div className='navbar navbar-dark bg-dark mb-4 px-4'>
             <span className='navbar-brand'>
-                <i className='fas fa-calendar-alt mx-2'></i> Andrés
+                <i className='fas fa-calendar-alt mx-2'></i> { user.name }
             </span>
-            <button className='btn btn-outline-danger'>
+            <button
+                className='btn btn-outline-danger'
+                onClick={ startLogout }
+            >
                 <i className='fas fa-sign-out-alt mx-2'></i>
                 <span>Salir</span>
             </button>
