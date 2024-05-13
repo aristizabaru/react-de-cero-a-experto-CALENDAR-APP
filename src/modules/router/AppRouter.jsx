2e9edecf17c10855838d40d5aae1e674bba40f1c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-12 20:23:23 -0500

    feat (AppRouter.jsx): implementación de validación de rutas
    Se implementa validación de rutas y redirección según estado de autenticación

--- src/modules/router/AppRouter.jsx
+++ src/modules/router/AppRouter.jsx
@@ -1,20 +1,43 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { CalendarPage } from '../calendar';
+import { useAuthStore } from '../shared';
+import { useEffect } from 'react';
 
 export const AppRouter = () => {
 
-    const authStatus = 'authenticated'; //'not-authenticated'; 
+    const { status, checkAuthToken } = useAuthStore();
+
+    useEffect( () => {
+        checkAuthToken();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [] );
+
+
+    if ( status === 'checking' ) {
+        return (
+            <h3>Cargando...</h3>
+        );
+    }
 
     return (
         <Routes>
             {
-                ( authStatus === 'not-authenticated' )
-                    ? <Route path='auth/*' element={ <LoginPage /> } />
-                    : <Route path='*' element={ <CalendarPage /> } />
+                ( status === 'not-authenticated' )
+                    ? (
+                        <>
+                            <Route path='auth/*' element={ <LoginPage /> } />
+                            <Route path='*' element={ <Navigate to='/auth/login' /> } />
+                        </>
+                    )
+                    : (
+                        <>
+                            <Route path='/' element={ <CalendarPage /> } />
+                            <Route path='*' element={ <Navigate to='/' /> } />
+                        </>
+                    )
             }
-            {/* { Fallback route } */ }
-            <Route path='*' element={ <Navigate to='/auth/login' /> } />
+
         </Routes>
     );
 };
