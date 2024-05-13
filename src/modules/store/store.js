f74b46aa50c20a1e5a2f1f58f4439a3b64266ccf
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-05-12 20:23:59 -0500

    feat (store.js): implementación de auth reducer
    Se adiciona el authSlide.reducer al store

--- src/modules/store/store.js
+++ src/modules/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { calendarSlice, uiSlice } from './';
+import { authSlice, calendarSlice, uiSlice } from './';
 
 export const store = configureStore( {
     reducer: {
+        auth: authSlice.reducer,
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer,
     },
